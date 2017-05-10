b968bd725f69dc2bcf35cab939f23a6c487ac724
JunaidGool <junaidgool@outlook.com>
2017-05-10 15:03:06 +0200

    Test 1 pass - count String

--- avo.js
+++ avo.js
@@ -1,6 +1,11 @@
-module.exports = function avo(){
+module.exports = function avo(avoString){
 
+  // var avoString  = "1 for R3, 2 for R7, 3 for R10, 5 for R14.50.";
+  var avoArray = avoString.split(",");
+  var avoCountResult = avoString.split(",").length;
 
+  console.log(avoCountResult);
 
-  
+  return avoCountResult;
+
 };
--- test/avo.tests.js
+++ test/avo.tests.js
@@ -1,7 +1,10 @@
-const avo = require('..avo');
+const avo = require('../avo');
 const assert = require('assert');
 
+describe('avo function', function(){
 
-
+    it('returns a number within a string', function(){
+        assert.equal(4, avo("1 for R3, 2 for R7, 3 for R10, 5 for R14.50."));
+    });
 
 });
